245b56b17ab554c29b6410ef3055e49419f72ecd
nimesha206 <121599526+nimesha206@users.noreply.github.com>
2024-03-14 19:02:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94767939688', 'Dinuwa Official²⁰²³', true],
-  ['94767939688', 'Dinuwa Official²⁰²³', true], 
+  ['94789549564', 'nimesha Official²⁰²³', true],
+  ['94789549564', 'nimesha Official²⁰²³', true], 
   [''] 
 ] //Number of owners
 
